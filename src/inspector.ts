c7aa885816f6c74fe437aca100aa454b298a56fa
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2019-02-18 14:53:16 +0300

    Pixify compilation process

---
+++ src/inspector.ts
@@ -0,0 +1,7 @@
+export {default as PixiInspector} from './PixiInspector';
+export {default as domAttr} from './decorators/domAttr';
+export {default as domLeaf} from './decorators/domLeaf';
+export {default as domHidden} from './decorators/domHidden';
+export {default as PrimitiveAttributeParser} from './attributes/PrimitiveAttributeParser';
+export {default as PointAttributeParser} from './attributes/PointAttributeParser';
+export {default as ArrayAttributeParser} from './attributes/ArrayAttributeParser';
