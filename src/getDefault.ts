0f3c9f26ff5e9c91f184851d3678c9dd62efecbb
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2019-02-19 12:27:37 +0300

    Added getDefault function

---
+++ src/getDefault.ts
@@ -0,0 +1,18 @@
+import {PixiInspector} from "./PixiInspector";
+import {PointAttributeParser} from "./attributes/PointAttributeParser";
+import {TextureAttributeParser} from "./attributes/TextureAttributeParser";
+
+export function getDefault(rootNode: PIXI.Container, canvas: HTMLCanvasElement) {
+    return new PixiInspector(rootNode, canvas)
+        .domAttr(PIXI.DisplayObject, 'x')
+        .domAttr(PIXI.DisplayObject, 'y')
+        .domAttr(PIXI.DisplayObject, 'scale', PointAttributeParser)
+        .domAttr(PIXI.DisplayObject, 'rotation')
+        .domAttr(PIXI.Sprite, 'texture', TextureAttributeParser)
+        .domAttr(PIXI.Sprite, 'anchor', PointAttributeParser)
+        .domAttr(PIXI.mesh.Mesh, 'texture', TextureAttributeParser)
+        .domAttr(PIXI.Text, 'text')
+        .domAttr(PIXI.Text, 'anchor', PointAttributeParser)
+        .domAttr(PIXI.extras.BitmapText, 'text')
+        .domAttr(PIXI.extras.BitmapText, 'anchor', PointAttributeParser);
+}
