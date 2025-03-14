39be8431062f3bdf148107486f80a5841e7d920f
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2025-03-14 19:18:25 +0300

    Added color variables

--- src/StyleSheet.ts
+++ src/StyleSheet.ts
@@ -1,3 +1,14 @@
+const textColorLight = "#202124";
+const textColorDark = "#e8eaed";
+const bgColorLight = "#fff";
+const bgColorDark = "#292a2d";
+const borderColorLight = "#dadce0";
+const borderColorDark = "#3c4043";
+const hoverColorLight = "#c8c8c9";
+const hoverColorDark = "#4b4c4f";
+const branchColorLight = "#72777c";
+const branchColorDark = "#8b9196";
+
 // language=CSS
 export const StyleSheet = `
     .pixi-inspector-context-menu,
@@ -11,9 +22,9 @@ export const StyleSheet = `
     .pixi-inspector-context-menu {
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         font-size: 12px;
-        color: #202124;
-        background: #fff;
-        border: 1px solid #dadce0;
+        color: ${textColorLight};
+        background: ${bgColorLight};
+        border: 1px solid ${borderColorLight};
         box-shadow: 4px 4px 3px -1px rgba(0, 0, 0, 0.5);
         line-height: 1.2;
         max-height: 100vh;
@@ -39,15 +50,16 @@ export const StyleSheet = `
         padding: 0 1.4em;
         white-space: nowrap;
         cursor: pointer;
+        position: relative;
     }
 
-    .pixi-inspector-context-menu li label span {
+    .pixi-inspector-context-menu li label > span {
         padding: 0.3em 0;
         display: inline-block;
     }
 
     .pixi-inspector-context-menu li label:hover {
-        background: #c8c8c9;
+        background: ${hoverColorLight};
     }
 
     .pixi-inspector-context-menu ul > li > .branch {
@@ -56,7 +68,7 @@ export const StyleSheet = `
         left: 0;
         height: 100%;
         width: 0.8em;
-        border-left: #72777c 1px dotted;
+        border-left: ${branchColorLight} 1px dotted;
         pointer-events: none;
     }
 
@@ -66,7 +78,7 @@ export const StyleSheet = `
         top: 0.9em;
         left: 0;
         width: 100%;
-        border-top: #72777c 1px dotted;
+        border-top: ${branchColorLight} 1px dotted;
     }
 
     .pixi-inspector-context-menu ul > li:last-child > .branch {
@@ -74,7 +86,7 @@ export const StyleSheet = `
     }
 
     .pixi-inspector-context-menu li[data-visible=false] {
-        color: #72777c;
+        color: ${branchColorLight};
     }
 
     .pixi-inspector-context-menu span[data-texture] u {
@@ -86,30 +98,30 @@ export const StyleSheet = `
         object-fit: contain;
         object-position: center;
         pointer-events: none;
-        background-image: url("data:image/svg+xml;utf8,${encodeURIComponent('<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><rect height="12" width="12" y="0" x="12" fill="#dadce0"/><rect height="12" width="12" y="12" x="0" fill="#dadce0"/></svg>')}");
+        background-image: url("data:image/svg+xml;utf8,${encodeURIComponent(`<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><rect height="12" width="12" y="0" x="12" fill="${borderColorLight}"/><rect height="12" width="12" y="12" x="0" fill="${borderColorLight}"/></svg>`)}");
     }
 
     .pixi-inspector-texture-popup-dark {
-        background-image: url("data:image/svg+xml;utf8,${encodeURIComponent('<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><rect height="12" width="12" y="0" x="12" fill="#3c4043"/><rect height="12" width="12" y="12" x="0" fill="#3c4043"/></svg>')}");
+        background-image: url("data:image/svg+xml;utf8,${encodeURIComponent(`<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><rect height="12" width="12" y="0" x="12" fill="${borderColorDark}"/><rect height="12" width="12" y="12" x="0" fill="${borderColorDark}"/></svg>`)}");
     }
 
     .pixi-inspector-context-menu-dark,
     .pixi-inspector-texture-popup-dark {
-        color: #e8eaed;
-        background-color: #292a2d;
-        border-color: #3c4043;
+        color: ${textColorDark};
+        background-color: ${bgColorDark};
+        border-color: ${borderColorDark};
     }
 
     .pixi-inspector-context-menu-dark li label:hover {
-        background-color: #4b4c4f;
+        background-color: ${hoverColorDark};
     }
 
     .pixi-inspector-context-menu-dark ul > li > .branch,
     .pixi-inspector-context-menu-dark ul > li > .branch:before {
-        border-color: #8b9196;
+        border-color: ${branchColorDark};
     }
 
     .pixi-inspector-context-menu-dark li[data-visible=false] {
-        color: #8b9196;
+        color: ${branchColorDark};
     }
 `;
